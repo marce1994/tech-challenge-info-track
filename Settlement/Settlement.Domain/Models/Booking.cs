1ec48b8aaabba57edf603a4c408962940ae73de3
Pablo Bianco <358126+marce1994@users.noreply.github.com.>
2024-05-17 20:26:22 +1000

    deleted old folders

--- Settlement/Settlement.Domain/Models/Booking.cs
+++
@@ -1,8 +0,0 @@
-namespace Settlement.Domain.Models;
-
-public class Booking
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = "";
-    public TimeOnly BookingTime { get; set; }
-}
